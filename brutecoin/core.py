58fd1a05b5609e01180e177e5a0e53d18927a1b6
Victor Usachev <usachev-1991@yandex.ru>
2019-07-19 06:23:21 +1000

    feat(core): Add private key generation
    The private key is created from a hexadecimal number, which is chosen
    randomly from a certain range.

--- brutecoin/core.py
+++ brutecoin/core.py
@@ -1,2 +1,21 @@
+import random
+
+
+def format_as_hex(number, length=64):
+    return f'{hex(number)[2:]:0>{length}}'
+
+
+def generate_hex_in_range(start, stop):
+    return format_as_hex(random.randrange(start, stop + 1))
+
+
+def generate_private_keys(start, stop, limit=None):
+    num = 0
+    while limit is None or num < limit:
+        yield generate_hex_in_range(start, stop)
+        num += 1
+
+
 def processing(args):
-    pass
+    for key in generate_private_keys(1, 256, 10):
+        print(key)
