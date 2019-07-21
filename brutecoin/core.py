e52e1c9ba0b52ce340f0e291fa77966356752ebc
Victor Usachev <usachev-1991@yandex.ru>
2019-07-22 04:52:28 +1000

    feat(core): Add address generation for specified private keys
    By default, the address is generated for a compressed key.

--- brutecoin/core.py
+++ brutecoin/core.py
@@ -1,6 +1,17 @@
 from multiprocessing import Process
 
+import bitcoin
+
 from brutecoin.utils import generate_hex_in_range, split_range
+
+
+def addresses_generator(keys):
+    yield from map(bitcoin.privtoaddr, keys)
+
+
+def read_keys(keys_path):
+    with open(keys_path) as fp:
+        yield from (line.strip() for line in fp.readlines())
 
 
 def key_generator(start, stop, limit=None):
@@ -8,6 +19,18 @@ def key_generator(start, stop, limit=None):
     while limit is None or num < limit:
         yield generate_hex_in_range(start, stop)
         num += 1
+
+
+def addresses_processing(keys_path, compressed=True):
+    keys = read_keys(keys_path)
+    if compressed:
+        keys = (bitcoin.encode_privkey(k, 'wif_compressed')
+                for k in keys)
+
+    generator = addresses_generator(keys)
+
+    for address in generator:
+        print(address)
 
 
 def keys_processing(keys_path, limit, start, stop):
@@ -22,23 +45,27 @@ def keys_processing(keys_path, limit, start, stop):
 
 
 def processing(args):
-    # Split the range between all processes
-    ranges = split_range(args.start, args.stop, args.processes)
+    if args.mode == 'pk':
+        # Split the range between all processes
+        ranges = split_range(args.start, args.stop, args.processes)
 
-    processes = [
-        Process(
-            target=keys_processing,
-            kwargs=dict(
-                keys_path=args.keys_path,
-                limit=args.limit,
-                start=start,
-                stop=stop
-            ))
-        for start, stop in ranges
-    ]
+        processes = [
+            Process(
+                target=keys_processing,
+                kwargs=dict(
+                    keys_path=args.keys_path,
+                    limit=args.limit,
+                    start=start,
+                    stop=stop
+                ))
+            for start, stop in ranges
+        ]
 
-    for process in processes:
-        process.start()
+        for process in processes:
+            process.start()
 
-    for process in processes:
-        process.join()
+        for process in processes:
+            process.join()
+    elif args.mode == 'addr':
+        assert args.keys_path
+        addresses_processing(args.keys_path)
