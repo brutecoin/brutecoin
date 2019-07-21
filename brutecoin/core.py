7e9cae281ca4527a1685d4a5526a547aa423b4be
Victor Usachev <usachev-1991@yandex.ru>
2019-07-22 00:18:21 +1000

    refactor(core): Move utility function into utils module

--- brutecoin/core.py
+++ brutecoin/core.py
@@ -1,12 +1,4 @@
-import random
-
-
-def format_as_hex(number, length=64):
-    return f'{hex(number)[2:]:0>{length}}'
-
-
-def generate_hex_in_range(start, stop):
-    return format_as_hex(random.randrange(start, stop + 1))
+from brutecoin.utils import generate_hex_in_range
 
 
 def key_generator(start, stop, limit=None):
