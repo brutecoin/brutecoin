f28a32759ef90d9631b34f74a2a0a0f8c1d93b60
Victor Usachev <usachev-1991@yandex.ru>
2020-02-20 01:42:59 +0300

    fix(utils): max_parts cannot be less then 1

--- brutecoin/utils.py
+++ brutecoin/utils.py
@@ -10,9 +10,12 @@ def generate_hex_in_range(start, stop):
     return format_as_hex(random.randrange(start, stop + 1))
 
 
-def split_range(start, stop, parts):
-    width = math.ceil((stop - start + 1) / parts)
+def split_range(start, stop, max_parts):
+    if max_parts < 1:
+        raise ValueError("max_parts cannot be less then 1")
+
+    width = math.ceil((stop - start + 1) / max_parts)
 
     while start <= stop:
         yield (start, min(start + width - 1, stop))
-        start = start + width
+        start += width
