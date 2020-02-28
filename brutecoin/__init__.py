5d3e413e7b8c06918d2b5897676bbaa54dc7b9f0
Victor Usachev <usachev-1991@yandex.ru>
2020-02-28 04:18:10 +0300

    fix(core): add keys_path parameter validation

--- brutecoin/__init__.py
+++ brutecoin/__init__.py
@@ -3,6 +3,10 @@ from pathlib import Path
 
 from .core import processing
 
+MODE_ADDR = 'addr'
+MODE_PK = 'pk'
+MODE_CHOICES = (MODE_ADDR, MODE_PK)
+
 
 def int_or_hex(string):
     if isinstance(string, str):
@@ -15,16 +19,13 @@ def main(argv):
     arg_parser = ArgumentParser(
         prog='brutecoin',
         description='Application for brute-force private keys and the '
-                    'corresponding addresses of Bitcoin puzzle transaction',
-        add_help=False
+        'corresponding addresses of Bitcoin puzzle transaction',
+        add_help=False,
     )
 
     # Positional argument
     arg_parser.add_argument(
-        'mode',
-        type=str,
-        choices=['addr', 'pk'],
-        help='limit of the keys number'
+        'mode', type=str, choices=MODE_CHOICES, help='limit of the keys number'
     )
 
     # Optional arguments
@@ -33,37 +34,37 @@ def main(argv):
         dest='keys_path',
         type=Path,
         default=None,
-        help='keys file path'
+        help='keys file path',
     )
 
     arg_parser.add_argument(
         '--limit',
         type=int,
         default='1',
-        help='limit of the keys number per process'
+        help='limit of the keys number per process',
     )
 
     arg_parser.add_argument(
-        '--processes',
-        type=int,
-        default='1',
-        help='number of processes'
+        '--processes', type=int, default='1', help='number of processes'
     )
 
     arg_parser.add_argument(
         '--start',
         type=int_or_hex,
         default='1',
-        help='lower bound of private key range'
+        help='lower bound of private key range',
     )
 
     arg_parser.add_argument(
         '--stop',
         type=int_or_hex,
         default='1',
-        help='upper bound of private key range'
+        help='upper bound of private key range',
     )
 
     args = arg_parser.parse_args(argv)
+
+    if args.mode == MODE_ADDR and not args.keys_path:
+        raise ValueError('In the specified mode, `keys_path` is required')
 
     processing(args)
